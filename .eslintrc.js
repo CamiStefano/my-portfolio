38e677508a06b181c0b678a792f0d4c1ec9fddec
Camila Victoria Stefano <66892573+CamiStefano@users.noreply.github.com>
2023-08-12 01:58:12 +0000

    add Airbnb config

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,23 +3,23 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["next/core-web-vitals", "airbnb", "plugin:prettier/recommended"],
+  extends: ['airbnb'],
   overrides: [
     {
       env: {
         node: true,
       },
-      files: [".eslintrc.{js,cjs}"],
+      files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
-        sourceType: "script",
+        sourceType: 'script',
       },
     },
   ],
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
   rules: {
-    "prettier/prettier": "warn",
+    'react/prop-types': 'off',
   },
 };
--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -1,6 +1,8 @@
+import React from 'react';
+
 export const metadata = {
-  title: "Mis proyectos - Cami Stefano",
-  description: "Proyectos de Cami Stefano",
+  title: 'Mis proyectos - Cami Stefano',
+  description: 'Proyectos de Cami Stefano',
 };
 
 export default function Projects() {
