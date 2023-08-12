940067bbb6d45fcfc97175312ad2b5b61dbea2a4
Camila Victoria Stefano <66892573+CamiStefano@users.noreply.github.com>
2023-08-12 00:48:58 +0000

    feat: :white_check_mark: add project setup

---
+++ app/projects/page.jsx
@@ -0,0 +1,9 @@
+export const metadata = {
+    title: 'Mis proyectos - Cami Stefano',
+    description: 'Proyectos de Cami Stefano',
+  }
+  
+
+export default function Page(){
+    return <h1>Mis proyectos</h1>
+}
