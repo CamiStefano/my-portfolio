36f6d3f612e557d21ff192272f1510caff01a3cd
Camila Victoria Stefano <66892573+CamiStefano@users.noreply.github.com>
2023-08-12 02:42:11 +0000

    feat: feat: add results-summary-component page

---
+++ app/projects/results-summary-component/page.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const metadata = {
+  title: 'Results summary component',
+  description: 'Proyectos de Cami Stefano',
+};
+
+export default function ResultsSummary() {
+  return <h1>Results summary component</h1>;
+}
