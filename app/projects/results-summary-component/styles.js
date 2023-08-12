3607a2ce73208fa249a5c533c016b22a1da060a9
Cami Stefano <camilavictoriastefano@gmail.com>
2023-08-12 18:46:55 -0300

    fix: Double quotes warning

--- app/projects/results-summary-component/styles.js
+++ app/projects/results-summary-component/styles.js
@@ -1,11 +1,11 @@
-"use client";
+'use client';
 
 import styled from 'styled-components';
 
-export const Wrapper  = styled.section``;
+export const Wrapper = styled.section``;
 
 export const Title = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    color: #bff;
+  font-size: 1.5em;
+  text-align: center;
+  color: #bff;
 `;
