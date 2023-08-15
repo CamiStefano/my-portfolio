a4525b869468d33f01de7a56513e5513c6fe8029
Cami Stefano <cami.stefano@live.com.ar>
2023-08-15 17:42:15 -0300

    update styles

---
+++ app/projects/results-summary-component/components/Button.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Button } from '../styles';
+
+export default function ResultsSummary() {
+  return <Button className='button'>Continue</Button>;
+}
--- app/projects/results-summary-component/styles.js
+++ app/projects/results-summary-component/styles.js
@@ -5,11 +5,12 @@ import styled from 'styled-components';
 const theme = {
   white: '#FFFFFF',
   red: '#FF5555',
-  lightGray: 'hsl(212, 45%, 89%)',
+  yelow: '#FFB21E',
+  green: '#00BB8F',
+  blue: '#1125D6',
   lightBlue: '#CAC9FF',
-  grayishBlue: 'hsl(220, 15%, 55%)',
-  darkBlue: 'hsl(218, 44%, 22%)',
   darkNavy: '#303B59',
+  gradient1: 'linear-gradient(180deg, #75F 0%, #6943FF 0.01%, #2F2CE9 100%);',
 };
 
 export const Wrapper = styled.section`
@@ -35,7 +36,7 @@ export const Card = styled.section`
     display: flex;
     flex-direction: column;
     align-items: center;
-    background: linear-gradient(180deg, #75f 0%, #6943ff 0.01%, #2f2ce9 100%);
+    background: ${theme.gradient1};
   }
 
   & .result__title {
@@ -104,20 +105,99 @@ export const Card = styled.section`
     padding-bottom: 28px;
   }
 
-  & .summary__reaction {
+  & .summary__reaction__cards {
     width: 288px;
     background: linear-gradient(0deg, rgba(255, 255, 255, 0.95) 0%, rgba(255, 255, 255, 0.95) 100%),
       #f55;
     color: ${theme.darkNavy};
     display: flex;
+    justify-content: space-between;
+    margin-bottom: 16px;
   }
 
   & .summary__reaction__img {
     padding-right: 12px;
+    vertical-align: middle;
   }
 
   & .summary__reaction__p {
     color: ${theme.red};
+    font-weight: 500;
+    font-size: 18px;
+  }
+
+  & .summary__number {
+    color: ${theme.darkNavy};
+    font-weight: 700;
+  }
+
+  & .summary__number__total {
+    color: ${theme.darkNavy};
+    font-weight: 700;
+    opacity: 0.5;
+  }
+
+  & .summary__memory__img {
+    padding-right: 12px;
+    vertical-align: middle;
+  }
+
+  & .summary__memory__p {
+    color: ${theme.yelow};
+    font-weight: 500;
+    font-size: 18px;
+  }
+
+  & .summary__memory__cards {
+    width: 288px;
+    background: linear-gradient(0deg, rgba(255, 255, 255, 0.95) 0%, rgba(255, 255, 255, 0.95) 100%),
+      #ffb21e;
+    color: ${theme.darkNavy};
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 16px;
+  }
+
+  & .summary__verbal__img {
+    padding-right: 12px;
+    vertical-align: middle;
+  }
+
+  & .summary__verbal__p {
+    color: ${theme.green};
+    font-weight: 500;
+    font-size: 18px;
+  }
+
+  & .summary__verbal__cards {
+    width: 288px;
+    background: linear-gradient(0deg, rgba(255, 255, 255, 0.95) 0%, rgba(255, 255, 255, 0.95) 100%),
+      #00bb8f;
+    color: ${theme.darkNavy};
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 16px;
+  }
+
+  & .summary__visual__img {
+    padding-right: 12px;
+    vertical-align: middle;
+  }
+
+  & .summary__visual__p {
+    color: ${theme.blue};
+    font-weight: 500;
+    font-size: 18px;
+  }
+
+  & .summary__visual__cards {
+    width: 288px;
+    background: linear-gradient(0deg, rgba(255, 255, 255, 0.95) 0%, rgba(255, 255, 255, 0.95) 100%),
+      #1125d6;
+    color: ${theme.darkNavy};
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 41px;
   }
 `;
 
@@ -134,4 +214,21 @@ export const Title = styled.h2`
   line-height: 31px;
   font-weight: 700;
   padding-top: 38px;
+`;
+
+export const Button = styled.button`
+  width: 288px;
+  height: 56px;
+  border-radius: 128px;
+  border: none;
+  background: ${theme.darkNavy};
+  color: ${theme.white};
+  font-size: 18px;
+  font-weight: 700;
+  cursor: pointer;
+  transition: .3 linear;
+
+  &.button:hover {
+    background: ${theme.gradient1};
+  }
 `;
