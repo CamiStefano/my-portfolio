7684453d114c4d1048b1c77f0e93046471ea2c3a
Cami Stefano <camilavictoriastefano@gmail.com>
2023-08-27 22:57:08 -0300

    add NavBar Component

---
+++ app/projects/skilled-elearning-landing-page/components/NavBar.jsx
@@ -0,0 +1,51 @@
+'use client';
+
+import React from 'react';
+import { styled } from 'styled-components';
+import { theme } from '../styles';
+import { LogoDark } from '../icons';
+import Button from './Button';
+
+const StyledNavBar = styled.footer`
+  width: 100%;
+  height: 7.5rem;
+  background-color: transparent;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  & .container {
+    width: 100%;
+    max-width: 1110px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    & .button {
+      background: ${theme.dark};
+      transition: all 0.4s ease;
+
+      &:hover {
+        background: ${theme.lightBlue};
+      }
+    }
+  }
+
+  @media (max-width: 768px) {
+    width: 19.5rem;
+  }
+`;
+
+function NavBar() {
+  return (
+    <StyledNavBar className="navbar">
+      <div className="container">
+        <LogoDark />
+
+        <Button className="button">Get Started</Button>
+      </div>
+    </StyledNavBar>
+  );
+}
+
+export default NavBar;
