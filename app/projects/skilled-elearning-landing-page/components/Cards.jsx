5401d55a99f411f1c6fbe4996d05137cb47f111b
camila-stefano <camilavictoriastefano@gmail.com>
2023-08-30 16:13:48 -0300

    style: :art: uodate styles Cards component

--- app/projects/skilled-elearning-landing-page/components/Cards.jsx
+++ app/projects/skilled-elearning-landing-page/components/Cards.jsx
@@ -1,32 +1,67 @@
 'use client';
 
+/* eslint-disable max-len */
+
+import Link from 'next/link';
 import React from 'react';
 import { styled } from 'styled-components';
 import { theme } from '../styles';
 
 const StyledCard = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
   width: 21.875rem;
   height: 20.125rem;
-  padding: 4rem;
   border-radius: 0.9375rem;
   background: ${theme.light};
-  padding: 4rem 2rem 8rem;
+  padding: 4rem 2rem 2.5rem;
+  position: relative;
 
-  & p {
-    max-width: 17.875rem;
+  & .img {
+    position: absolute;
+    top: -1.5rem;
+  }
+
+  & .subtitle {
     color: ${theme.dark};
-    font-size: 2rem;
+    font-size: 1.5rem;
     font-style: normal;
     font-weight: 800;
-    line-height: 2.5rem;
+    line-height: 2rem;
+  }
+
+  & .label {
+    color: ${theme.grey};
+    font-size: 1.125rem;
+    font-style: normal;
+    font-weight: 500;
+    line-height: 1.75rem;
+  }
+
+  & .link {
+    color: ${theme.pink};
+    font-size: 1.125rem;
+    font-style: normal;
+    font-weight: 600;
+    line-height: 1.75rem;
+    transition: all 0.5s ease;
+
+    &:hover {
+      color: ${theme.lightPink};
+    }
   }
 `;
 
-function Cards() {
+function Cards({ icon, subtitle, label }) {
   return (
     <StyledCard className="boxes">
-      <p>asjaksjaksjajsajks</p>
-      <p>asjaksjaksjajsajks</p>
+      <span className="img">{icon}</span>
+      <h2 className="subtitle">{subtitle}</h2>
+      <p className="label">{label}</p>
+      <Link className="link" href="/projects/skilled-elearning-landing-page/">
+        Get Started
+      </Link>
     </StyledCard>
   );
 }
--- app/projects/skilled-elearning-landing-page/components/CardsSection.jsx
+++ app/projects/skilled-elearning-landing-page/components/CardsSection.jsx
@@ -4,11 +4,14 @@ import React from 'react';
 import { styled } from 'styled-components';
 import Cards from './Cards';
 import { theme } from '../styles';
+import { AnimationSVG, DesignSVG, PhotographySVG, CryptoSVG, BusinessSVG } from '../icons';
 
 const StyledCardsSection = styled.section`
   display: flex;
   justify-content: space-around;
   flex-wrap: wrap;
+  gap: 1.8rem;
+  padding: 0rem 0rem 7rem;
 
   & .box-1 {
     width: 21.875rem;
@@ -33,11 +36,11 @@ function CardsSection() {
       <div className="box-1">
         <p>Check out our most popular courses!</p>
       </div>
-      <Cards />
-      <Cards />
-      <Cards />
-      <Cards />
-      <Cards />
+      <Cards icon={<AnimationSVG />} subtitle="Animation" label="Learn the latest animation techniques to create stunning motion design and captivate your audience." />
+      <Cards icon={<DesignSVG />} subtitle="Design" label="Create beautiful, usable interfaces to help shape the future of how the web looks." />
+      <Cards icon={<PhotographySVG />} subtitle="Photography" label="Explore critical fundamentals like lighting, composition, and focus to capture exceptional photos." />
+      <Cards icon={<CryptoSVG />} subtitle="Crypto" label="All you need to know to get started investing in crypto. Go from beginner to advanced with this 54 hour course." />
+      <Cards icon={<BusinessSVG />} subtitle="Business" label="A step-by-step playbook to help you start, scale, and sustain your business without outside investment." />
     </StyledCardsSection>
   );
 }
